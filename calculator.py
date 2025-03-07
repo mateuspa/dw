1b6527a3adac0ffe87958ea0d915a2de382ee9d6
mateuspa <mateus.prince@gmail.com>
2025-03-07 09:16:21 -0300

    aula 3 - testes html e CSS, integração GIT, GITHUB e VERCEL

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#importando bibliotecas
+import time
+
+#Entrada de Dados
+x=int(input("Digite o número X: "))
+y=int(input("Digite o número Y: "))
+
+#Cálculos Matemáticos
+z=x+y
+
+#Saída de Dados
+print(z)
+time.sleep(3)
